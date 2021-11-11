0d29bd5837eb5e421fbebb7bdbff63f4ca8dfb6b
Dmitry <elxf123@gmail.com>
2021-11-11 21:51:00 +0200

    все одним солюшином

---
+++ Task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿/* За заданим восьмизначним натуральним числом знаходить середнє арифметичне цифр цього числа та кількість ненульових цифр. */
+
+using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter eight-digit natural number: ");
+            var userNum = Console.ReadLine();
+            var counter = 0;
+            double sum = 0;
+
+            foreach (char ch in userNum)
+            {
+                var number = char.GetNumericValue(ch);
+                //для char TryParse не працює, воно вимагає ReadOnlySpan<char>. А вот GetNumericValue хорошо, но вертає double, того sum я зразу і оголосив як double
+                sum += number;
+                counter += Convert.ToInt32(!number.Equals(0));
+            }
+
+            sum /= userNum.Length;
+            Console.WriteLine($"Number is {userNum}. \n Avarage is {sum}. \n Non-zero numbers: {counter}.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Визначити і надрукувати номери і значення двох чисел Фібоначі, в проміжок між якими потрапляє введене з клавіатури натуральне число. */
+
+using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a natural number: ");
+            var userNum = Convert.ToInt32(Console.ReadLine());
+            var fibCounter = 0;
+            var getFib = 0;
+
+            /* Рахуємо числа Фібоначі до тих пір, поки наше число не стане більше, ніж число користувача. fib_counter дасть нам номер Фібоначі, більшого за число юзера */
+            while (getFib <= userNum)
+            {
+                fibCounter++;
+                getFib = Figiovanna(fibCounter);
+            }
+
+            /* При цьому, якшо юзер ввів число Фібоначі, то найближче знизу буде мати номер (fib_counter - 2). Якщо ж user_num != числу Фібоначі, то найближче знизу матиме номер (fib_counter - 2) */
+            if (Figiovanna(fibCounter - 1) == userNum)
+            // В System.Linq є метод Equals. Треба було його в if-виразі юзати? Якшо да, то чого звичайне == гірше?
+            {
+                Console.WriteLine($"Upper Fibonacci number is: {Figiovanna(fibCounter)}. Lower Fibonacci number is: {Figiovanna(fibCounter - 2)}.");
+            }
+            else
+            {
+                Console.WriteLine($"Upper Fibonacci number is: {Figiovanna(fibCounter)}. Lower Fibonacci number is: {Figiovanna(fibCounter - 1)}.");
+            }
+
+
+            Console.ReadLine();
+        }
+
+        /* Метод для обрахунку ряду Фібоначі */
+        static int Figiovanna(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return Figiovanna(n - 1) + Figiovanna(n - 2);
+
+        }
+    }
+}
