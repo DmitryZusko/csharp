18665ad5a6704b41c569e0ec8c096e3c7091f437
Dmitry <elxf123@gmail.com>
2022-01-07 17:12:53 +0200

    Add task 7

---
+++ Task_7/Program.cs
@@ -0,0 +1,39 @@
+﻿/* 7.Звичайний консольний застосунок.З текстового файлу зчитати послідовність записів, які містять дані про результати сесії студентів групи у такому форматі: 
+ * <Прізвище>, <Ім’я>, <Дата народження>, <Список екзаменаційних оцінок>. Роздрукувати введені дані у вигляді таблиці, а також роздрукувати список студентів, 
+ * які отримали лише оцінки 5 на іспитах, у зростаючому порядку за віком */
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Jonathan = new Student("Joestar", "Jonathan", "04.04.1868", 5.0);
+            var Joseph = new Student("Joestar", "Joseph", "27.09.1920", 5.0);
+            var Jotaro = new Student("Kujo", "Jotaro", "03.04.1970", 5.0);
+            var Josuke = new Student("Higashikata", "Josuke", "20.05.1983", 5.0);
+            var Giorno = new Student("Giovanna", "Giorno", "16.04.1985", 5.0);
+            var Jolyne = new Student("Kujo", "Jolyne", "15.12.1992", 5.0);
+            var Johnny = new Student("Joestar", "Johnny", "10.04.1872", 4.9);
+            var Lexa = new Student("Gay-Sailor", "Lexa", "19.08.2011", 4.9);
+
+            var allStudents = new List<Student> { Johnny, Jotaro, Joseph, Jonathan, Josuke, Giorno, Jolyne, Lexa };
+
+            Console.WriteLine("*** Let's see a list of students *** \n");
+            Student.PrintAllStudents(allStudents);
+
+            Console.WriteLine("\n *** Now let's find and sort best Jojos *** \n");
+
+            Student.GetMaxAndSort(allStudents);
+
+        }
+    }
+}
---
+++ Task_7/Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task_7
+{
+    class Student
+    {
+        private string _lastName;
+        private string _firstName;
+        private DateTime _birthday;
+        private double _averageMark;
+        private static int stCount;
+        private static int stMaxMarkCount;
+
+        public Student(string lastName, string FirstName, string Birthday, double AverageMark)
+        {
+            _lastName = lastName;
+            _firstName = FirstName;
+            _birthday = DateTime.Parse(Birthday);
+            _averageMark = AverageMark;
+            stCount++;
+            if (_averageMark == 5.0) stMaxMarkCount++;
+
+        }
+
+        private void Print()
+        {
+            Console.WriteLine($"{_lastName} \t {_firstName} \t {_birthday} \t {_averageMark}");
+        }
+
+        static public void PrintAllStudents(List<Student> stList)
+        {
+            foreach (Student student in stList)
+                student.Print();
+        }
+
+
+        private static Student[] SortByBirthday(Student[] arr)
+        {
+            for (var i = 1; i < arr.Length; i++)
+            {
+                for (var j = 0; j < arr.Length - i; j++)
+                {
+
+                    if (DateTime.Compare(arr[j]._birthday, arr[j+1]._birthday) > 0)
+                    {
+                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                    }
+
+                }
+            }
+
+            return arr;
+        }
+
+        public static void GetMaxAndSort(List<Student> stList)
+        {
+            List<Student> bestStudents = new List<Student>();
+
+            foreach (Student student in stList)
+            {
+                if (student._averageMark == 5.0)
+                    bestStudents.Add(student);
+            }
+
+            Student[] sortBestSt = Student.SortByBirthday(bestStudents.ToArray());
+            Student.PrintAllStudents(sortBestSt.ToList());
+        }
+
+    }
+}
