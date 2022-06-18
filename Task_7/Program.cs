e539b0351b1db696703e754af380d428562d80ff
Dmitry <elxf123@gmail.com>
2022-06-18 18:56:31 +0300

    1) Now program read info from file 2) Remade method GetMaxAndSort(List<Student> stList) 3) Minor code cleaning

--- Task_7/Program.cs
+++ Task_7/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Globalization;
 
 
 
@@ -16,23 +17,21 @@ namespace Task_7
     {
         static void Main(string[] args)
         {
-            var Jonathan = new Student("Joestar", "Jonathan", "04.04.1868", 5.0);
-            var Joseph = new Student("Joestar", "Joseph", "27.09.1920", 5.0);
-            var Jotaro = new Student("Kujo", "Jotaro", "03.04.1970", 5.0);
-            var Josuke = new Student("Higashikata", "Josuke", "20.05.1983", 5.0);
-            var Giorno = new Student("Giovanna", "Giorno", "16.04.1985", 5.0);
-            var Jolyne = new Student("Kujo", "Jolyne", "15.12.1992", 5.0);
-            var Johnny = new Student("Joestar", "Johnny", "10.04.1872", 4.9);
-            var Lexa = new Student("Gay-Sailor", "Lexa", "19.08.2011", 4.9);
 
-            var allStudents = new List<Student> { Johnny, Jotaro, Joseph, Jonathan, Josuke, Giorno, Jolyne, Lexa };
+            var studentsList = new List<Student>();
+            foreach (var line in File.ReadLines("Students.txt"))
+            {
+                var splitStudent = line.Split(", ");
+                studentsList.Add(new Student(splitStudent[0], splitStudent[1], splitStudent[2], double.Parse(splitStudent[3])));
+            }
+
 
             Console.WriteLine("*** Let's see a list of students *** \n");
-            Student.PrintAllStudents(allStudents);
+            Student.PrintAllStudents(studentsList);
 
             Console.WriteLine("\n *** Now let's find and sort best Jojos *** \n");
 
-            Student.GetMaxAndSort(allStudents);
+            Student.GetMaxAndSort(studentsList);
 
         }
     }
--- Task_7/Student.cs
+++ Task_7/Student.cs
@@ -11,8 +11,6 @@ namespace Task_7
         private string _firstName;
         private DateTime _birthday;
         private double _averageMark;
-        private static int stCount;
-        private static int stMaxMarkCount;
 
         public Student(string lastName, string FirstName, string Birthday, double AverageMark)
         {
@@ -20,14 +18,12 @@ namespace Task_7
             _firstName = FirstName;
             _birthday = DateTime.Parse(Birthday);
             _averageMark = AverageMark;
-            stCount++;
-            if (_averageMark == 5.0) stMaxMarkCount++;
 
         }
 
         private void Print()
         {
-            Console.WriteLine($"{_lastName} \t {_firstName} \t {_birthday} \t {_averageMark}");
+            Console.WriteLine($"{_lastName} \t {_firstName} \t {_birthday.ToShortDateString()} \t {_averageMark}");
         }
 
         static public void PrintAllStudents(List<Student> stList)
@@ -36,37 +32,12 @@ namespace Task_7
                 student.Print();
         }
 
-
-        private static Student[] SortByBirthday(Student[] arr)
-        {
-            for (var i = 1; i < arr.Length; i++)
-            {
-                for (var j = 0; j < arr.Length - i; j++)
-                {
-
-                    if (DateTime.Compare(arr[j]._birthday, arr[j+1]._birthday) > 0)
-                    {
-                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
-                    }
-
-                }
-            }
-
-            return arr;
-        }
-
         public static void GetMaxAndSort(List<Student> stList)
         {
             List<Student> bestStudents = new List<Student>();
 
-            foreach (Student student in stList)
-            {
-                if (student._averageMark == 5.0)
-                    bestStudents.Add(student);
-            }
-
-            Student[] sortBestSt = Student.SortByBirthday(bestStudents.ToArray());
-            Student.PrintAllStudents(sortBestSt.ToList());
+            Student[] sortedBestSt = stList.Where(student => student._averageMark >= 5.0).OrderBy(student => student._birthday).ToArray();
+            Student.PrintAllStudents(sortedBestSt.ToList());
         }
 
     }
